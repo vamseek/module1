8202d848c2c15720d2163efce3a5ad604361ec68
Vamsee M Kamabathula <vamsee@vamseek.com>
2019-06-29 17:56:57 +0530

    initial import

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+export interface IName {
+    firstName: string;
+
+    lastName: string;
+}
+
+export interface ITransaction {
+    date: Date;
+
+    buyer: string;
+
+    seller: string;
+}
+
+export let demo = false;
